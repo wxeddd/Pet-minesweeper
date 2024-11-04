d2f1929752ce19ef632cb39065ccaddf07a608da
wxeddd <mrdavek9@gmail.com>
2024-11-04 10:56:11 +0200

    added enum and grid was made colorful

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <windows.h>
 
 using namespace std;
+
+enum Color
+{
+    COLOR_BLACK,
+    COLOR_BLUE,
+    COLOR_GREEN,
+    COLOR_AQUA,
+    COLOR_RED,
+    COLOR_PURPLE,
+    COLOR_YELLOW,
+    COLOR_DEFAULT,
+    COLOR_GRAY,
+    COLOR_LIGHTBLUE,
+    COLOR_LIGHTGREEN,
+    COLOR_LIGHTAQUA,
+    COLOR_LIGHTRED,
+    COLOR_LIGHTPURPLE,
+    COLOR_LIGHTYELLOW,
+    COLOR_LIGHTWHITE
+};
+
+void setColor(int textColor);
 void printGrid(char **arr, size_t length);
 char **createArray(size_t length);
 void deleteArray(char **arr, size_t length);
@@ -13,38 +38,58 @@ int main()
     auto a = createArray(length);
     printGrid(a, length);
     deleteArray(a, length);
-
+    system("pause");
 }
 
 void printGrid(char **arr, size_t length)
 {
     char ROWS = 'A';
     int COLS = 1;
-    cout<<endl<<"    ";
-    for(size_t i = 0; i<length;i++)
+    cout << endl << "   ";
+
+    setColor(COLOR_LIGHTYELLOW); 
+    for (size_t i = 0; i < length; i++)
     {
-        cout<<COLS++<<"   ";
-        
+        int width = 4;
+        string numStr = to_string(COLS);
+        int leftPadding = (width - numStr.size()) / 2;
+        int rightPadding = width - numStr.size() - leftPadding;
+        cout << setw(leftPadding + numStr.size()) << COLS++
+             << setw(rightPadding) << " "; 
     }
-    for (size_t i = 0; i < length ; i++)
+    setColor(COLOR_DEFAULT);
+
+    for (size_t i = 0; i < length; i++)
     {
-        cout<<endl<<"  +";
-        for(size_t k = 0; k< length ;k++)
+        setColor(COLOR_LIGHTRED);
+        cout << endl
+             << "  +";
+        for (size_t k = 0; k < length; k++)
         {
-            cout<<"---+";
+            cout << "---+";
         }
-        cout<<endl;
-        cout<< ROWS++<<" |";
-        for (size_t j = 0; j <length ; j++)
+        cout << endl;
+        setColor(COLOR_LIGHTYELLOW);
+        cout << ROWS++;
+        setColor(COLOR_RED);
+        cout << " |";
+        setColor(COLOR_DEFAULT);
+        for (size_t j = 0; j < length; j++)
         {
-            cout<<" "<<arr[i][j]<<" |"; 
+            cout << " " << arr[i][j]; 
+            setColor(COLOR_LIGHTRED);
+            cout<<" |";
         }
     }
-    cout<<endl<<"  +";
-    for(size_t k = 0; k< length ;k++)
-        {
-            cout<<"---+";
-        }
+
+    setColor(COLOR_LIGHTRED);
+    cout << endl << "  +";
+    
+    for (size_t k = 0; k < length; k++)
+    {
+        cout << "---+";
+    }
+    setColor(COLOR_DEFAULT);
 }
 char **createArray(size_t length)
 {
@@ -61,9 +106,33 @@ char **createArray(size_t length)
 }
 void deleteArray(char **arr, size_t length)
 {
-    for(size_t i = 0; i < length; i++)
+    for (size_t i = 0; i < length; i++)
     {
         delete[] arr[i];
     }
     delete[] arr;
 }
+void setColor(int textColor)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, textColor);
+}
+/*
+    0 = Black
+    1 = Blue
+    2 = Green
+    3 = Aqua
+    4 = Red
+    5 = Purple
+    6 = Yellow
+    7 = White
+    8 = Gray
+    9 = Light Blue
+    10 = Light Green
+    11 = Light Aqua
+    12 = Light Red
+    13 = Light Purple
+    14 = Light Yellow
+    15 = Bright White
+*/
+// add progress board:  Time: 00:45 | Flags left: 3 | Moves: 10.
--- minesweepertesting.cpp
+++ minesweepertesting.cpp
@@ -2,8 +2,31 @@
 #include <string>
 #include <iomanip>
 #include <cstdlib>
+#include <windows.h>
 
 using namespace std;
+
+enum Color
+{
+    COLOR_BLACK,
+    COLOR_BLUE,
+    COLOR_GREEN,
+    COLOR_AQUA,
+    COLOR_RED,
+    COLOR_PURPLE,
+    COLOR_YELLOW,
+    COLOR_DEFAULT,
+    COLOR_GRAY,
+    COLOR_LIGHTBLUE,
+    COLOR_LIGHTGREEN,
+    COLOR_LIGHTAQUA,
+    COLOR_LIGHTRED,
+    COLOR_LIGHTPURPLE,
+    COLOR_LIGHTYELLOW,
+    COLOR_LIGHTWHITE
+};
+
+void setColor(int textColor);
 void printGrid(char **arr, size_t length);
 char **createArray(size_t length);
 void deleteArray(char **arr, size_t length);
@@ -24,6 +47,7 @@ void printGrid(char **arr, size_t length)
     int COLS = 1;
     cout << endl << "   ";
 
+    setColor(COLOR_LIGHTYELLOW); 
     for (size_t i = 0; i < length; i++)
     {
         int width = 4;
@@ -31,11 +55,13 @@ void printGrid(char **arr, size_t length)
         int leftPadding = (width - numStr.size()) / 2;
         int rightPadding = width - numStr.size() - leftPadding;
         cout << setw(leftPadding + numStr.size()) << COLS++
-             << setw(rightPadding) << " ";
+             << setw(rightPadding) << " "; 
     }
+    setColor(COLOR_DEFAULT);
 
     for (size_t i = 0; i < length; i++)
     {
+        setColor(COLOR_LIGHTRED);
         cout << endl
              << "  +";
         for (size_t k = 0; k < length; k++)
@@ -43,19 +69,27 @@ void printGrid(char **arr, size_t length)
             cout << "---+";
         }
         cout << endl;
-        cout << ROWS++ << " |";
+        setColor(COLOR_LIGHTYELLOW);
+        cout << ROWS++;
+        setColor(COLOR_RED);
+        cout << " |";
+        setColor(COLOR_DEFAULT);
         for (size_t j = 0; j < length; j++)
         {
-            cout << " " << arr[i][j] << " |";
+            cout << " " << arr[i][j]; 
+            setColor(COLOR_LIGHTRED);
+            cout<<" |";
         }
     }
 
+    setColor(COLOR_LIGHTRED);
     cout << endl << "  +";
     
     for (size_t k = 0; k < length; k++)
     {
         cout << "---+";
     }
+    setColor(COLOR_DEFAULT);
 }
 char **createArray(size_t length)
 {
@@ -78,3 +112,27 @@ void deleteArray(char **arr, size_t length)
     }
     delete[] arr;
 }
+void setColor(int textColor)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, textColor);
+}
+/*
+    0 = Black
+    1 = Blue
+    2 = Green
+    3 = Aqua
+    4 = Red
+    5 = Purple
+    6 = Yellow
+    7 = White
+    8 = Gray
+    9 = Light Blue
+    10 = Light Green
+    11 = Light Aqua
+    12 = Light Red
+    13 = Light Purple
+    14 = Light Yellow
+    15 = Bright White
+*/
+// add progress board:  Time: 00:45 | Flags left: 3 | Moves: 10.
