b19f6bf1f6a5f45f239e628bd63ce8cac5d40a18
wxeddd <mrdavek9@gmail.com>
2024-11-05 01:14:04 +0200

    Modified color output approach for better performance, more intuitive and cross-platform-ability. Optimized method of printing grid Added comments(proly useless due to further implementations)

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -3,30 +3,45 @@
 #include <iomanip>
 #include <cstdlib>
 #include <windows.h>
+#include <unordered_map>
 
 using namespace std;
 
-enum Color
-{
-    COLOR_BLACK,
-    COLOR_BLUE,
-    COLOR_GREEN,
-    COLOR_AQUA,
-    COLOR_RED,
-    COLOR_PURPLE,
-    COLOR_YELLOW,
-    COLOR_DEFAULT,
-    COLOR_GRAY,
-    COLOR_LIGHTBLUE,
-    COLOR_LIGHTGREEN,
-    COLOR_LIGHTAQUA,
-    COLOR_LIGHTRED,
-    COLOR_LIGHTPURPLE,
-    COLOR_LIGHTYELLOW,
-    COLOR_LIGHTWHITE
+unordered_map<string, string> setColor = {                          // unordered map for easier and more intuitive use of ANSI Escape Codes to manage output color
+    {"DEFAULT", "\033[0m"},
+    {"BLACK", "\033[30m"},
+    {"RED", "\033[31m"},
+    {"GREEN", "\033[32m"},
+    {"YELLOW", "\033[33m"},
+    {"BLUE", "\033[34m"},
+    {"MAGENTA", "\033[35m"},
+    {"CYAN", "\033[36m"},
+    {"WHITE", "\033[37m"},
+    {"GRAY", "\033[90m"},
+    {"RED_BRIGHT", "\033[91m"},
+    {"GREEN_BRIGHT", "\033[92m"},
+    {"YELLOW_BRIGHT", "\033[93m"},
+    {"BLUE_BRIGHT", "\033[94m"},
+    {"MAGENTA_BRIGHT", "\033[95m"},
+    {"CYAN_BRIGHT", "\033[96m"},
+    {"WHITE_BRIGHT", "\033[97m"},
+    {"BLACK_BG", "\033[40m"},
+    {"RED_BG", "\033[41m"},
+    {"GREEN_BG", "\033[42m"},
+    {"YELLOW_BG", "\033[43m"},
+    {"BLUE_BG", "\033[44m"},
+    {"MAGENTA_BG", "\033[45m"},
+    {"CYAN_BG", "\033[46m"},
+    {"WHITE_BG", "\033[47m"},
+    {"GRAY_BRIGHT_BG", "\033[100m"},
+    {"RED_BRIGHT_BG", "\033[101m"},
+    {"GREEN_BRIGHT_BG", "\033[102m"},
+    {"YELLOW_BRIGHT_BG", "\033[103m"},
+    {"BLUE_BRIGHT_BG", "\033[104m"},
+    {"MAGENTA_BRIGHT_BG", "\033[105m"},
+    {"CYAN_BRIGHT_BG", "\033[106m"},
+    {"WHITE_BRIGHT_BG", "\033[107m"}
 };
-
-void setColor(int textColor);
 void printGrid(char **arr, size_t length);
 char **createArray(size_t length);
 void deleteArray(char **arr, size_t length);
@@ -37,63 +52,50 @@ int main()
     const size_t length = lengthlite;
     auto a = createArray(length);
     printGrid(a, length);
-    deleteArray(a, length);
     system("pause");
+    deleteArray(a, length);
 }
 
-void printGrid(char **arr, size_t length)
+void printGrid(char **arr, size_t length)                           //  function to print grid
 {
     char ROWS = 'A';
     int COLS = 1;
-    cout << endl << "   ";
-
-    setColor(COLOR_LIGHTYELLOW); 
-    for (size_t i = 0; i < length; i++)
+    cout << endl
+         << "   ";
+    for (size_t i = 0; i < length; i++)                             //  Print COLUMN numbers for better orientation in the grid
     {
-        int width = 4;
-        string numStr = to_string(COLS);
+        int width = 4;                                              //added pudding so COLUMN numbers wouldn`t shift when their lenght is longer than 1 symbol
+        string numStr = to_string(COLS);                            
         int leftPadding = (width - numStr.size()) / 2;
         int rightPadding = width - numStr.size() - leftPadding;
-        cout << setw(leftPadding + numStr.size()) << COLS++
-             << setw(rightPadding) << " "; 
+        cout << setColor["YELLOW_BRIGHT"] << setw(leftPadding + numStr.size()) << COLS++ << setw(rightPadding) << " " << setColor["DEFAULT"]; //output COLUMN number
     }
-    setColor(COLOR_DEFAULT);
-
-    for (size_t i = 0; i < length; i++)
+    string horizontalMesh = "  +";                                  
+    for (int i = 0; i < length; i++)                                //  added horizontalMesh for easier grid output and optimize number of cout calls
     {
-        setColor(COLOR_LIGHTRED);
-        cout << endl
-             << "  +";
-        for (size_t k = 0; k < length; k++)
-        {
-            cout << "---+";
-        }
-        cout << endl;
-        setColor(COLOR_LIGHTYELLOW);
-        cout << ROWS++;
-        setColor(COLOR_RED);
-        cout << " |";
-        setColor(COLOR_DEFAULT);
-        for (size_t j = 0; j < length; j++)
-        {
-            cout << " " << arr[i][j]; 
-            setColor(COLOR_LIGHTRED);
-            cout<<" |";
-        }
+        horizontalMesh += "---+";                                   // making horizontalMesh string as long, as the grid
     }
-
-    setColor(COLOR_LIGHTRED);
-    cout << endl << "  +";
     
-    for (size_t k = 0; k < length; k++)
+    cout << endl;                       
+    for (size_t i = 0; i < length; i++)                             //  printing grid`s row
     {
-        cout << "---+";
+        cout << setColor["RED_BRIGHT"] << horizontalMesh << endl;   //  printing horizontal barrier
+        cout << setColor["YELLOW_BRIGHT"] << ROWS++;                //  printing ROW`s number for better orientation in the grid                
+        cout <<setColor["RED_BRIGHT"]<< " |";                       //  printing side border between ROW number and first cell
+        for (size_t j = 0; j < length; j++)                         //  printing row
+        {
+            cout << setColor["DEFAULT"]<< " " << arr[i][j];                               //  printing cell`s content
+            cout << setColor["RED_BRIGHT"]<< " |";                //  printing border between cells
+        }
+        cout << endl;                                               //  new line 
     }
-    setColor(COLOR_DEFAULT);
+
+    cout << setColor["RED_BRIGHT"] << horizontalMesh << setColor["DEFAULT"] << endl;       //  printing bottom horizontal border
 }
-char **createArray(size_t length)
+
+char **createArray(size_t length)                                   //  function to return matrix
 {
-    char **arr = new char *[length];
+    char **arr = new char *[length];                                //  create an array
     for (size_t i = 0; i < length; i++)
     {
         arr[i] = new char[length];
@@ -104,6 +106,7 @@ char **createArray(size_t length)
     }
     return arr;
 }
+
 void deleteArray(char **arr, size_t length)
 {
     for (size_t i = 0; i < length; i++)
@@ -112,27 +115,5 @@ void deleteArray(char **arr, size_t length)
     }
     delete[] arr;
 }
-void setColor(int textColor)
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, textColor);
-}
-/*
-    0 = Black
-    1 = Blue
-    2 = Green
-    3 = Aqua
-    4 = Red
-    5 = Purple
-    6 = Yellow
-    7 = White
-    8 = Gray
-    9 = Light Blue
-    10 = Light Green
-    11 = Light Aqua
-    12 = Light Red
-    13 = Light Purple
-    14 = Light Yellow
-    15 = Bright White
-*/
-// add progress board:  Time: 00:45 | Flags left: 3 | Moves: 10.
+
+    // add progress board:  Time: 00:45 | Flags left: 3 | Moves: 10.
